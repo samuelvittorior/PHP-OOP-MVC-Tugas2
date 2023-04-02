2d53c273d0bfce5fd660aad684d615b9f84bad21
samuelvittorior <129597319+samuelvittorior@users.noreply.github.com>
2023-04-02 23:30:39 +0700

    Add files via upload

---
+++ Lat4_3a.php
@@ -0,0 +1,28 @@
+<?php
+
+class Mahasiswa {
+    private $nama;
+    private $nim;
+    
+    function __construct() {}    
+    
+    function setNama($a) {
+        $this->nama = $a;
+    }
+    
+    function setNim($b) {
+        $this->nim = $b;
+    }
+
+    function getNama() {
+        return $this->nama;
+    }
+    
+    function getNim() {
+        return $this->nim;
+    }
+
+    function __destruct() {}
+}
+
+?>
---
+++ Lat4_3b.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("Lat4_3a.php");
+
+$mhs1 = new Mahasiswa();
+$mhs1->setNama("hendra");
+echo $mhs1->getNama();;
+
+?>
