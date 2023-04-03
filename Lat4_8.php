1b2c31d5ddadb33f9ac53197b4193a1f53944602
samuelvittorior <129597319+samuelvittorior@users.noreply.github.com>
2023-04-04 04:13:33 +0700

    Add files via upload

---
+++ Lat4_8.php
@@ -0,0 +1,19 @@
+<?php
+
+class One {
+
+private static $var=0;
+
+        function __construct() {}
+
+        static function disp() {
+
+                print self::$var;
+            }
+
+            function __destruct() {}
+}
+
+One::disp();
+
+?>
